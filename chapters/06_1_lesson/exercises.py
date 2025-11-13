0183017bc5004b12254fa15aeef328493d7f9c5c
JoshKnickrehm <joshknickrehm1000@gmail.com>
2025-11-13 09:53:22 -0500

    week 13 work

--- chapters/06_1_lesson/exercises.py
+++ chapters/06_1_lesson/exercises.py
@@ -8,37 +8,34 @@ print("********** Ch 6 Exercise 2 **********")
 
 
 print("********** Ch 6 Exercise 3 **********")
-
 print("********** Ch 6 Exercise 4 **********")
 
 def is_power(b, c):
     if c == 1:
+        print('It is a power of 2')
         return True
+        
     elif c == b:
+        print('It is a power of 2') 
         return True
+        
     elif c > b:
         print(int(c))
         return is_power(b, c/b)
     else:
+        print('it is not a power of 2')
         return False
 
-while True:
-    big_number = int(input('Give me a number'))
-    result = is_power(2, big_number)
-    print (result)
-
-
-
-
-
-
-
-print("Ch 6 Exercise 4: Not implemented") # Delete this line when you write your code!
-
-
-
+# while True:
+#     big_number = int(input('Give me a number '))
+#     result = is_power(2, big_number)
+#     print (result)
 print("********** Ch 6 Exercise 5 **********")
 
-# Do your work for Exercise 5 here.
-
-print("Ch 6 Exercise 5: Not implemented") # Delete this line when you write your code!
+def GCD(a,b):
+    if b == 0:
+        return a
+    else:
+        return GCD(b,a % b)
+    
+print(GCD(15,25))
